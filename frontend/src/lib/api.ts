1be08a6202f530d7f964750b9b4ed68630275226
mewadah49-droid <mewadah49@gmail.com>
2026-02-01 04:04:11 +0530

    chore: add response data and final URL logging to API response interceptor

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -45,6 +45,10 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   (response) => {
     console.log('API Response:', response.status, response.config.url)
+    console.log('API Data:', response.data)
+    if (response.request?.responseURL) {
+      console.log('Final URL:', response.request.responseURL)
+    }
     return response
   },
   (error) => {
