1b6740e4a1718770f9f19651da8a9005686a9e6a
mewadah49-droid <mewadah49@gmail.com>
2026-02-01 05:54:38 +0530

    feat: Add JWT authentication to REST Framework default authentication classes.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -116,7 +116,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # REST Framework
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
